64a0b244b5796550a50f0c667221f90ed3758eec
DanilAndreev <danssg08@gmail.com>
2024-07-15 14:33:45 +0300

    [SCAR] JSON Input support WIP

--- SCAR/cli/SCARCLI.cpp
+++ SCAR/cli/SCARCLI.cpp
@@ -94,6 +94,7 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> globalDefinesS;
     std::vector<std::string> includeDirectoriesS;
     SCAR::CompileSettings settings{};
+    std::filesystem::path psoDescFilepath = {};
     try {
         CLI::App app{"RHINO Shader Compiler-Archiver", "SCAR"};
         app.add_option("-o,--out", outFilepath, "Out object filepath. If not set - result will be printed to STDOUT.");
@@ -113,11 +114,30 @@ int main(int argc, char* argv[]) {
         app.add_option("-I", "Define shader macro.")->take_all()->each([&includeDirectoriesS](const std::string& val) {
             includeDirectoriesS.push_back(val);
         });
+        app.add_option("desc", psoDescFilepath, "PSO Desc json filepath.");
         app.parse(argc, argv);
     }
     catch (const std::exception& e) {
         std::cerr << "SCAR Failed: " << e.what() << std::endl;
     }
+
+    std::ifstream psoDescFile{psoDescFilepath};
+    assert(psoDescFile.is_open());
+    JSONDescView descView{nlohmann::json::parse(psoDescFile)};
+    settings.psoType = descView.GetCompileSettingsView().psoType;
+    settings.rootSignature = descView.GetCompileSettingsView().rootSignature;
+    switch (settings.psoType) {
+        case SCAR::ArchivePSOType::Compute:
+            settings.computeSettings = descView.GetCompileSettingsView().computeSettings;
+            break;
+        case SCAR::ArchivePSOType::Graphics:
+            settings.graphicsSetting = descView.GetCompileSettingsView().graphicsSetting;
+            break;
+        case SCAR::ArchivePSOType::Library:
+            settings.librarySettings = descView.GetCompileSettingsView().librarySettings;
+            break;
+    }
+    psoDescFile.close();
 
     std::vector<const char*> globalDefines{};
     globalDefines.resize(globalDefinesS.size());
@@ -135,36 +155,36 @@ int main(int argc, char* argv[]) {
     settings.includeDirectoriesCount = includeDirectories.size();
     settings.includeDirectories = includeDirectories.data();
 
-    // -------------------------- START TESTS
-    const RHINO::DescriptorRangeDesc space0rd[] = {
-        RHINO::DescriptorRangeDesc{RHINO::DescriptorRangeType::SRV, 0, 1},
-        RHINO::DescriptorRangeDesc{RHINO::DescriptorRangeType::UAV, 1, 1},
-        RHINO::DescriptorRangeDesc{RHINO::DescriptorRangeType::CBV, 2, 1},
-    };
-
-    const RHINO::DescriptorSpaceDesc spaces[] = {
-        RHINO::DescriptorSpaceDesc{0, 0, std::size(space0rd), space0rd},
-    };
-
-    SCAR::PSORootSignatureDesc rootSignature{};
-    rootSignature.spacesCount = std::size(spaces);
-    rootSignature.spacesDescs = spaces;
-
-    const char* entrypoints[] = {"MyRaygenShader", "MyMissShader", "MyClosestHitShader"};
-
-    settings.rootSignature = &rootSignature;
-    settings.psoLang = SCAR::ArchivePSOLang::DXIL;
-    settings.psoType = SCAR::ArchivePSOType::Library;
-    settings.librarySettings.entrypointsCount = std::size(entrypoints);
-    settings.librarySettings.entrypoints = entrypoints;
-    settings.librarySettings.inputFilepath = "rt.hlsl";
-    settings.librarySettings.maxAttributeSizeInBytes = 32;
-    settings.librarySettings.maxPayloadSizeInBytes = 32;
-
-
-    outFilepath = "out.scar";
-
-    //----------------------------END TESTS
+    // // -------------------------- START TESTS
+    // const RHINO::DescriptorRangeDesc space0rd[] = {
+    //     RHINO::DescriptorRangeDesc{RHINO::DescriptorRangeType::SRV, 0, 1},
+    //     RHINO::DescriptorRangeDesc{RHINO::DescriptorRangeType::UAV, 1, 1},
+    //     RHINO::DescriptorRangeDesc{RHINO::DescriptorRangeType::CBV, 2, 1},
+    // };
+    //
+    // const RHINO::DescriptorSpaceDesc spaces[] = {
+    //     RHINO::DescriptorSpaceDesc{0, 0, std::size(space0rd), space0rd},
+    // };
+    //
+    // SCAR::PSORootSignatureDesc rootSignature{};
+    // rootSignature.spacesCount = std::size(spaces);
+    // rootSignature.spacesDescs = spaces;
+    //
+    // const char* entrypoints[] = {"MyRaygenShader", "MyMissShader", "MyClosestHitShader"};
+    //
+    // settings.rootSignature = &rootSignature;
+    // settings.psoLang = SCAR::ArchivePSOLang::DXIL;
+    // settings.psoType = SCAR::ArchivePSOType::Library;
+    // settings.librarySettings.entrypointsCount = std::size(entrypoints);
+    // settings.librarySettings.entrypoints = entrypoints;
+    // settings.librarySettings.inputFilepath = "rt.hlsl";
+    // settings.librarySettings.maxAttributeSizeInBytes = 32;
+    // settings.librarySettings.maxPayloadSizeInBytes = 32;
+    //
+    //
+    // outFilepath = "out.scar";
+    //
+    // //----------------------------END TESTS
 
     const SCAR::CompilationResult res = SCAR::Compile(&settings);
 
