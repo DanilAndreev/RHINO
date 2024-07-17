0f855f825d8cc8e6487e3b5efdf1a3322a508221
DanilAndreev <danssg08@gmail.com>
2024-07-17 20:34:44 +0300

    [Runtime] Minor

--- Runtime/source/Metal/MetalDescriptorHeap.mm
+++ Runtime/source/Metal/MetalDescriptorHeap.mm
@@ -7,7 +7,7 @@
 
 namespace RHINO::APIMetal {
     void MetalDescriptorHeap::WriteSRV(const WriteBufferDescriptorDesc& desc) noexcept {
-        auto* metalBuffer = static_cast<MetalBuffer*>(desc.buffer);
+        auto* metalBuffer = INTERPRET_AS<MetalBuffer*>(desc.buffer);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         size_t bufferGPUAddress = [metalBuffer->buffer gpuAddress] + desc.bufferOffset;
         IRDescriptorTableSetBuffer(entry + desc.offsetInHeap, bufferGPUAddress, 0);
@@ -15,7 +15,7 @@ namespace RHINO::APIMetal {
     }
 
     void MetalDescriptorHeap::WriteUAV(const WriteBufferDescriptorDesc& desc) noexcept {
-        auto* metalBuffer = static_cast<MetalBuffer*>(desc.buffer);
+        auto* metalBuffer = INTERPRET_AS<MetalBuffer*>(desc.buffer);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         size_t bufferGPUAddress = [metalBuffer->buffer gpuAddress] + desc.bufferOffset;
         IRDescriptorTableSetBuffer(entry + desc.offsetInHeap, bufferGPUAddress, 0);
@@ -23,7 +23,7 @@ namespace RHINO::APIMetal {
     }
 
     void MetalDescriptorHeap::WriteCBV(const WriteBufferDescriptorDesc& desc) noexcept {
-        auto* metalBuffer = static_cast<MetalBuffer*>(desc.buffer);
+        auto* metalBuffer = INTERPRET_AS<MetalBuffer*>(desc.buffer);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         size_t bufferGPUAddress = [metalBuffer->buffer gpuAddress] + desc.bufferOffset;
         IRDescriptorTableSetBuffer(entry + desc.offsetInHeap, bufferGPUAddress, 0);
@@ -31,34 +31,38 @@ namespace RHINO::APIMetal {
     }
 
     void MetalDescriptorHeap::WriteSRV(const WriteTexture2DDescriptorDesc& desc) noexcept {
-        auto* metalTexture2D = static_cast<MetalTexture2D*>(desc.texture);
+        auto* metalTexture2D = INTERPRET_AS<MetalTexture2D*>(desc.texture);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         IRDescriptorTableSetTexture(entry + desc.offsetInHeap, metalTexture2D->texture, 0, 0);
         m_Resources[desc.offsetInHeap] = metalTexture2D->texture;
     }
 
     void MetalDescriptorHeap::WriteUAV(const WriteTexture2DDescriptorDesc& desc) noexcept {
-        auto* metalTexture2D = static_cast<MetalTexture2D*>(desc.texture);
+        auto* metalTexture2D = INTERPRET_AS<MetalTexture2D*>(desc.texture);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         IRDescriptorTableSetTexture(entry + desc.offsetInHeap, metalTexture2D->texture, 0, 0);
         m_Resources[desc.offsetInHeap] = metalTexture2D->texture;
     }
 
     void MetalDescriptorHeap::WriteSRV(const WriteTexture3DDescriptorDesc& desc) noexcept {
-        auto* metalTexture3D = static_cast<MetalTexture2D*>(desc.texture);
+        auto* metalTexture3D = INTERPRET_AS<MetalTexture2D*>(desc.texture);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         IRDescriptorTableSetTexture(entry + desc.offsetInHeap, metalTexture3D->texture, 0, 0);
         m_Resources[desc.offsetInHeap] = metalTexture3D->texture;
     }
 
     void MetalDescriptorHeap::WriteUAV(const WriteTexture3DDescriptorDesc& desc) noexcept {
-        auto* metalTexture3D = static_cast<MetalTexture2D*>(desc.texture);
+        auto* metalTexture3D = INTERPRET_AS<MetalTexture2D*>(desc.texture);
         auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
         IRDescriptorTableSetTexture(entry + desc.offsetInHeap, metalTexture3D->texture, 0, 0);
         m_Resources[desc.offsetInHeap] = metalTexture3D->texture;
     }
 
     void MetalDescriptorHeap::WriteSRV(const WriteTLASDescriptorDesc& desc) noexcept {
+        auto* metalTLAS = INTERPRET_AS<MetalTLAS*>(desc.tlas);
+        auto* entry = static_cast<IRDescriptorTableEntry*>([m_DescriptorHeap contents]);
+        // IRDescriptorTableSetAccelerationStructure(entry + desc.offsetInHeap, metalTLAS->accelerationStructure);
+        // m_Resources[desc.offsetInHeap] = metalTLAS->accelerationStructure;
         // TODO: implement
     }
 
