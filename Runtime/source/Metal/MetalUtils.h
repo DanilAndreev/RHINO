15c790c13f9dd9c1eeb9d3932096cae356b8c723
DanilAndreev <danssg08@gmail.com>
2024-09-26 07:35:32 +0300

    [Runtime] Metal MacOS15 feature

--- Runtime/source/Metal/MetalUtils.h
+++ Runtime/source/Metal/MetalUtils.h
@@ -7,14 +7,15 @@
  * @return true if event signaled value equals input value after sleep timeout and false if not.
  */
 inline bool WaitForMTLSharedEventValue(id<MTLSharedEvent> event, uint64_t value, size_t timeoutMS) noexcept {
-    // TODO: Uncommit after MacOS 15 [:harold:]
-    // return [event waitUntilSignaledValue:value timeoutMS:timeoutMS];
-
-    constexpr size_t SLEEP_SESSION_TIME_MS = 10;
-    size_t totalWaited = 0;
-    while (event.signaledValue != value && totalWaited < timeoutMS) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_SESSION_TIME_MS));
-        totalWaited += SLEEP_SESSION_TIME_MS;
+    if (@available(macOS 15.0, *)) {
+        return [event waitUntilSignaledValue:value timeoutMS:timeoutMS];
+    } else {
+        constexpr size_t SLEEP_SESSION_TIME_MS = 10;
+        size_t totalWaited = 0;
+        while (event.signaledValue != value && totalWaited < timeoutMS) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_SESSION_TIME_MS));
+            totalWaited += SLEEP_SESSION_TIME_MS;
+        }
+        return event.signaledValue == value;
     }
-    return event.signaledValue == value;
 }
